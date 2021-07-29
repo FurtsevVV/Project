84a54b9f989a8e5b98179e86777febdf3fc3b8b0
Valentin Furtsev <furtsevvv@gmail.com>
2021-07-29 06:20:11 +0600

    Test branch

---
+++ DemoStreamAPI.java
@@ -0,0 +1,29 @@
+package com.company;
+
+import java.io.IOException;
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class DemoStreamAPI {
+    public static void main(String[] args) throws IOException {
+//        String path  = "C:/Users/Zakat/AdvancedJava/src/com/company/resourses/hello.txt";
+//       byte [] bytes = Files.readAllBytes(Paths.get(path));
+//      String contents = new String(bytes, StandardCharsets.UTF_8);
+//    //    Stream<String> words = Arrays.stream(contents.split("\\PL+"),10,20);
+//
+//        List<String> words = Arrays.asList(contents.split("\\PL+"));
+//        Stream <String> upperWords = words.stream().map(c -> c.substring(1,2));
+//
+////long count = words.filter(w -> w.length() > 8).count();
+//        System.out.println((upperWords));
+
+
+
+
+    }
+}
