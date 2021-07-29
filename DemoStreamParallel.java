2be6619e79ad4d1e150059382bfaa9299af565ea
Valentin Furtsev <furtsevvv@gmail.com>
2021-07-29 06:24:58 +0600

    New Test to branch newnranch

--- DemoStreamParallel.java
+++ DemoStreamParallel.java
@@ -1,2 +1,25 @@
-package com.company;public class DemoStreamParallel {
-}
+package com.company;
+
+import javafx.print.Collation;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.*;
+
+public class DemoStreamParallel {
+
+
+    public static void main(String[] args) throws IOException {
+
+Stream.iterate(10, x -> x + 2).limit(5).forEach(System.out::println);
+
+        }
+
+    }
+
--- Phone.java
+++ Phone.java
@@ -1,2 +1,20 @@
-package com.company;public class Phone {
+package com.company;
+
+class Phone{
+
+    private String name;
+    private String company;
+    private int price;
+
+    public Phone(String name, String comp, int price){
+        this.name=name;
+        this.company=comp;
+        this.price = price;
+    }
+
+    public String getName() { return name; }
+    public int getPrice() { return price; }
+    public String getCompany() { return company; }
 }
+
+
