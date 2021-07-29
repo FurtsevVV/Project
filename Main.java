e5011584e8a2cfdaa2eea53a55cd45d71f96d5e9
Valentin Furtsev <furtsevvv@gmail.com>
2021-07-29 06:29:55 +0600

    Main.java

--- Main.java
+++ Main.java
@@ -1,22 +1,32 @@
 package com.company;
 
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.*;
 
 public class Main {
-        public static void main(String[] args) {
-
-                for (int i = 0; i < 1000; i++) {
-                System.out.println("Test motherfucker!");
-
-
+            public static void main(String[] args) throws IOException {
+                String path  = "C:/Users/Zakat/AdvancedJava/src/com/company/resourses/hello.txt";
+byte [] bytes = Files.readAllBytes(Paths.get(path));
+String contents = new String(bytes, StandardCharsets.UTF_8);
+String [] words = contents.split("\\PL+");
+int count = 0;
+for (String w: words) {
+    if (w.length() > 6) {
+        count++;
+    }
 }
 
-}
-public class Person {
-                private String name;
-}
-}
+                System.out.println(count);
+
+    }
+
+    }
+
 
 
 
